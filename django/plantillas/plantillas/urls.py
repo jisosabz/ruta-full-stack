03d6176fcd48d4dfa9ae9731cce295c3da96ee6e
jisosabz <jisosabz@users.noreply.github.com>
2025-03-27 23:17:34 -0300

    uso de contextos

--- django/plantillas/plantillas/urls.py
+++ django/plantillas/plantillas/urls.py
@@ -7,4 +7,5 @@ from plantillas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simple/',views.simple,name='simple'),
+    path('dinamico/<str:name>',views.dinamico,name='dinamico'),
 ]
--- django/plantillas/plantillas/views.py
+++ django/plantillas/plantillas/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 def simple(request):
     #tiene como parametros la request, la plantilla a cargar, contexto
     return render(request, "simple.html",{})
+def dinamico(request, name):
+    categories = ['code', 'design', 'marketing']
+    context = {'name': name, 'categories': categories}
+    return render(request, "dinamico.html",context)
+    #como contexto tambien se puede pasar varias cosas como objetos y demás
