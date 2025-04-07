680e6b1804df5e04ceeefc30e9e455964d0d4749
jisosabz <jisosabz@users.noreply.github.com>
2025-04-07 16:42:48 -0300

    modelos como configurar models.py

--- django/modularizacion/comentarios/models.py
+++ django/modularizacion/comentarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    score = models.IntegerField(default=3, null=False)
+    comment = models.TextField(max_length=1000, null=False)
+    date = models.DateField(null=True)
+    signature = models.TextField(max_length=1000, null=False, default='')
+    def __str__(self):
+        return self.name
