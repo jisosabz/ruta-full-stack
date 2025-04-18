9c0ef947d1b941093bbc3b7f1c56b87b7ab8e3b1
jisosabz <jisosabz@users.noreply.github.com>
2025-04-18 18:45:48 -0300

    terminando el modulo de contactos

--- django/agenda/contact/forms.py
+++ django/agenda/contact/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, EmailInput, TextInput, Textarea
 
 from contact.models import Contact
 
@@ -7,4 +7,13 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
-        widgets = {}
+        exclude = ('date',)
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
+            'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}),
+            'phone': TextInput(attrs={'class': 'form-control', 'placeholder': 'Teléfono'}),
+            'mobile': TextInput(attrs={'class': 'form-control', 'placeholder': 'Celular'}),
+            'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo electrónico'}),
+            'company': TextInput(attrs={'class': 'form-control', 'placeholder': 'Empresa'}),
+            'notes': Textarea(attrs={'class': 'form-control', 'placeholder': 'Notas', 'rows': 4}),
+        }
--- django/agenda/contact/views.py
+++ django/agenda/contact/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +11,51 @@ def index(request):
     return render(request, 'contact/index.html',{
         'contacts': contacts,
     })
+
+def view(request, id):
+    contact = Contact.objects.get(id=id)
+    return render(request, 'contact/details.html', {
+        'contact': contact,
+    })
+
+def edit(request, id):
+    contact = Contact.objects.get(id=id)
+    if request.method == 'GET':
+        form = ContactForm(instance=contact)
+        context = {
+            'form': form,
+            'id': id,
+        }
+        return render(request, 'contact/edit.html',context)
+    elif request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        form.save()
+        context = {
+            'form': form,
+            'id': id,
+        }
+        messages.success(request, 'Contacto Actualizado Correctamente')
+        return render(request, "contact/edit.html", context)
+    else:
+        return None
+
+def create(request):
+    if request.method == 'GET':
+        form = ContactForm()
+        context = {
+            'form': form,
+        }
+        return render(request, "contact/create.html", context)
+    elif request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('contact')
+    else:
+        return None
+
+
+def delete(request, id):
+    contact = Contact.objects.get(id=id)
+    contact.delete()
+    return redirect('contact')
